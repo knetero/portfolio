448e854c700d5135d04c94a02b3163cd02d2632d
Abdellah Azeroual <azero853@gmail.com>
2024-11-01 11:19:33 +0100

    fixed:navbar animation toggle

--- src/components/ui/background-lines.jsx
+++ src/components/ui/background-lines.jsx
@@ -1,12 +1,10 @@
 "use client";
 import { cn } from "@/lib/utils";
-import { motion } from "framer-motion";
 import React from "react";
 
 export const BackgroundLines = ({
   children,
   className,
-  svgOptions
 }) => {
   return (
     (<div
